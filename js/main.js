37acbbac814fbab7ad4eb09d5b489226d7bd24c3
h-hiroki <gyouzakuuka@gmail.com>
2019-08-13 15:59:53 +0900

    axiosでAPIをコールするところまで

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ new Vue({
   el: '#app',
   data: {
     bpi: null
+  },
+  mounted: function() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response) {
+      this.bpi = response.data.bpi
+    }.bind(this))
   }
 })
